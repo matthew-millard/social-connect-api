d00bb6df40ba1b551cd928a51102c12791ed73f6
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-08 00:32:02 -0400

    Feat: Add thought GET crud ops

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 import connection from '../config/connection.js';
 import models from '../models/index.js';
-const { User } = models;
+const { User, Thought } = models;
 import populateUsers from '../utils/generate-users.js';
+import populateThoughts from '../utils/generate-thoughts.js';
 
 connection.on('error', err => console.error(err));
 
@@ -9,14 +10,20 @@ connection.once('open', async () => {
   try {
     console.log('Connection made, starting seed...');
 
-    // Drop existing users
-    await User.deleteMany({});
+    // Drop all existing users and thoughts
+    await Promise.all([User.deleteMany({}), Thought.deleteMany({})]);
 
     // Generate user data
     const users = populateUsers();
 
     // Add users to the collection and await the results
     await User.collection.insertMany(users);
+
+    // Generate thought data
+    const thoughts = await populateThoughts();
+
+    // Add thoughts to the collection and await the results
+    await Thought.collection.insertMany(thoughts);
 
     console.log('Data seeded successfully 🌱');
     process.exit(0);
