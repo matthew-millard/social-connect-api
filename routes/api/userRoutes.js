0674185f27c9bcc269eba5c82b4290bce2d06001
matthew-millard <123032297+matthew-millard@users.noreply.github.com>
2023-07-07 21:42:24 -0400

    Feat: Add and remove friend from friend list

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,11 +1,22 @@
 import express from 'express';
 const router = express.Router();
-import { getUsers, getSingleUser, createUser, updateUser, deleteUser } from '../../controllers/userController.js';
+import {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} from '../../controllers/userController.js';
 
 // /api/users/
 router.route('/').get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').put(addFriend).delete(deleteFriend);
 
 export default router;
